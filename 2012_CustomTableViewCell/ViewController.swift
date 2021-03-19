2119e68f249e89467782c3e46fce3b4b0a60bc03
vikash4824 <vamsivikash24@gmail.com>
2021-03-19 10:22:18 +0530

    Initial Commit

---
+++ 2012_CustomTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2012_CustomTableViewCell
+//
+//  Created by mamidisetty Vikash on 19/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
